803ff3898f7e780772187c1c414287432e2af679
Nahda Raihan <raihannaida@gmail.com>
2023-09-07 15:32:46 +0530

    new function multiplication created

--- file1.js
+++ file1.js
@@ -3,8 +3,17 @@ function addNumbers(a,b){
 }
 let a=addNumbers(1,2)
 console.log(a)
+
+
+
 function substraction(a,b){
     return a-b
 }
 let c=substraction(10,4)
 console.log(c)
+
+function multiplication(a,b){
+    return a*b
+}
+let output=multiplication(10,2)
+console.log(output)
