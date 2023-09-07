74f484ce672762d5c48ba2adcd3b6c395a19b715
Nahda Raihan <raihannaida@gmail.com>
2023-09-07 15:25:14 +0530

    added new function substraction

--- file1.js
+++ file1.js
@@ -3,3 +3,8 @@ function addNumbers(a,b){
 }
 let a=addNumbers(1,2)
 console.log(a)
+function substraction(a,b){
+    return a-b
+}
+let c=substraction(10,4)
+console.log(c)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function substraction(a,b){
+    return a-b
+}
+let c=substraction(a,b)
+console.log(c)
